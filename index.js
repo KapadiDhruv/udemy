43754666dc7f838be400e9898e8cb8c6ae5ed8c5
meet.velani <meetvelani2728@gmail.com>
2022-07-13 10:49:05 +0530

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = requiree('express')
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('Hello')
+})
+
+app.listen(3000, () { 
+    console.log('http://localhost:3000')
+})
